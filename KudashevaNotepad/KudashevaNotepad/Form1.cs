96991e5e9fd265288b0b06442ac9c55f8883f0b1
Liana <lianalia2005@mail.ru>
2023-09-20 18:59:43 +0500

    форма оранж и длинная

--- KudashevaNotepad/KudashevaNotepad/Form1.cs
+++ KudashevaNotepad/KudashevaNotepad/Form1.cs
@@ -16,5 +16,10 @@ namespace KudashevaNotepad
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
