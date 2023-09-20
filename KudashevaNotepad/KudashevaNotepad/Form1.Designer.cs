0dcb1376fdd5991ce9a97c3f182f7ae62e8ebce7
Liana <lianalia2005@mail.ru>
2023-09-20 19:03:14 +0500

    оранж+длинн

--- KudashevaNotepad/KudashevaNotepad/Form1.Designer.cs
+++ KudashevaNotepad/KudashevaNotepad/Form1.Designer.cs
@@ -45,8 +45,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Yellow;
-            this.ClientSize = new System.Drawing.Size(658, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(1052, 450);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Блокнот";
